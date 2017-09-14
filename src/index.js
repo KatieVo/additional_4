51ae7100a43280bfcdb568fbbbb27c342baa81d4
KatieVo <31858127+KatieVo@users.noreply.github.com>
2017-09-14 17:22:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,116 @@
 module.exports = function multiply(first, second) {
-  return first*second;
+
+  let prev;
+  let result = [];
+  let finalRes = [];
+  let t = 2;
+  let pr = 0;
+  let res;
+  if (first.length < second.length) {
+    let v = first;
+    first = second;
+    second = v;
+  }
+  for (let i = second.length - 1; i >= 0; i--) {
+    let arr = [];
+    for (let j = first.length - 1; j >= 0; j--) {
+      let m = second[i] * first[j];
+
+      if (j === first.length - 1) {
+        if (m < 10) {
+          arr.unshift(m);
+          prev = 0;
+        } else {
+          arr.unshift(m % 10);
+          prev = Math.floor(m / 10);
+        }
+      } else {
+        if (j === 0) {
+          if (m + prev < 10) {
+            arr.unshift(m + prev);
+          } else {
+            arr.unshift((m + prev) % 10);
+            arr.unshift(Math.floor((m + prev) / 10));
+          }
+
+        } else if ((m + prev) < 10) {
+          arr.unshift(m + prev);
+          prev = Math.floor(m / 10);
+        } else {
+          arr.unshift((m + prev) % 10);
+          prev = Math.floor((m + prev) / 10);
+        }
+      }
+
+    }
+    if (i === second.length - 1) {
+      let pp = arr.pop();
+      finalRes.unshift(pp);
+      result = arr;
+    } else {
+      let k = result.length - 1;
+      let d = arr.length - 1;
+      // console.log(k, d);
+
+
+      if (d > k) {
+        for (k; k >= 0; k--) {
+          res = result[k] + arr[d] + pr;
+          if (res < 10) {
+            result[k] = res;
+            pr = 0;
+          } else {
+            result[k] = res % 10;
+            pr = Math.floor(res / 10);
+          }
+          d--;
+        }
+        while (d >= 0) {
+          if ((arr[d] + pr) < 10) {
+            result.unshift(arr[d] + pr);
+            pr = 0;
+            d--;
+          } else {
+            result.unshift((arr[d] + pr) % 10);
+            result.unshift(Math.floor((arr[d] + pr) / 10));
+            d--;
+          }
+
+
+        }
+        pp = result.pop();
+        finalRes.unshift(pp);
+        pr = 0;
+        // console.log('d in d>k', d);
+      } else {
+        for (k; k >= 0; k--) {
+          res = result[k] + arr[d] + pr;
+          if (res < 10) {
+            result[k] = res;
+            pr = 0;
+          } else {
+            result[k] = res % 10;
+            pr = Math.floor(res / 10);
+            if (k === 0) {
+              result.unshift(pr);
+              pr = 0;
+            }
+          }
+          d--;
+          //вот ту ошибка с d и с k какая-то
+        }
+        pp = result.pop();
+        finalRes.unshift(pp);
+        pr = 0;
+      }
+
+    }
+
+  }
+
+  finalRes = result.concat(finalRes);
+
+  return finalRes.join('');
+
 }
+
